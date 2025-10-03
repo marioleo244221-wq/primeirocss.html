5fac06bb1f655854d57d21026556f9873c2f0b95
MarioleoPR <marioleo244221@gmail.com>
2025-10-03 20:15:02 -0300

    primeiro js

---
+++ imags.html/arquivo.js
@@ -0,0 +1,12 @@
+const imput = document.querySelector('input');
+imput.addEventListener('keyup', function (event) {
+    if (event.key === "Enter") {
+        alert('Você pressionou a tecla Enter');
+    }
+});
+
+
+
+
+
+consol.log("")
